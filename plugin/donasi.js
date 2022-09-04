570402a8b29e780f8c24fd0d9512a3ce3b9d4043
KhamidBatang <111238475+KhamidBatang@users.noreply.github.com>
2022-09-04 12:43:55 +0700

    Update and rename plugins/donasi.js to plugin/donasi.js

--- plugin/donasi.js
+++ plugin/donasi.js
@@ -8,22 +8,22 @@ let handler = async (m, { conn }) => conn.send2ButtonLoc(m.chat, await (await fe
 ╠═══════════════════
 ║          ❉ 〔 *𝗗𝗢𝗡𝗔𝗦𝗜* 〕 ❉
 ║➸ *DANA* : 
-║➸ 085850539404
+║➸ 
 ║➸ *PULSA
 ║➸ 085850539404
 ║➸ *Saweria*:
-║ https://saweria.co/jarotoffc
+║ 
 ╰═══════════════════
 ╔════════════════════
 ║ *Donasi Via Ikuti Sosial media*
 ╠════════════════════
 ║     ❉ 〔 𝐒𝐨𝐬𝐢𝐚𝐥 𝐌𝐞𝐝𝐢𝐚 𝐉𝐚𝐫𝐨𝐭 〕 ❉
 ║➸ *Instasgram*
-║ https://instagram.com/Jarotr_
+║ https://instagram.com/khamid_serba_ada
 ║➸ *Youtube*
 ║ https://tinyurl.com/2qsejxnw
 ║➸ *Github*
-║ https://github.com/Alpiii22 
+║ https://github.com
 ╚════════════════════
 
 `.trim(), wm, 'Owner', '.owner', 'Menu', '.menu')
